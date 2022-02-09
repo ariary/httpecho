5f6e9a53a1fb28e49dc034478e8544f3e22e56b5
Ariary <ariary9.2@hotmail.fr>
2022-02-09 13:42:03 +0100

    Add tls

--- httpecho.go
+++ httpecho.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"bufio"
+	"crypto/tls"
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"net"
 	"os"
@@ -12,11 +14,12 @@ import (
 )
 
 const usage = `Usage of httpecho: echo server accepting malformed HTTP request
-  -s --serve serve continuously (default: wait for 1 request)
-  -t, --timeout timeout to close connection in millisecond. Needed for closing http request. (default: 500)
-  -d, --dump dump incoming request to a file (default: only print to stdout)
-  -p, --port listening on specific port (default: 8888)
-  -h, --help dump incoming request to a file (default: only print to stdout) 
+  -s --serve      serve continuously (default: wait for 1 request)
+  -t, --timeout   timeout to close connection in millisecond. Needed for closing http request. (default: 500)
+  -d, --dump      dump incoming request to a file (default: only print to stdout)
+  -p, --port      listening on specific port (default: 8888)
+  --tls           use TLS encryption for communication
+  -h, --help      dump incoming request to a file (default: only print to stdout) 
 `
 
 func main() {
@@ -40,19 +43,38 @@ func main() {
 	flag.StringVar(&port, "port", "8888", "Listening on specific port (default: 8888)")
 	flag.StringVar(&port, "p", "8888", "Listening on specific port (default: 8888)")
 
+	//--tls
+	var encrypted bool
+	flag.BoolVar(&encrypted, "tls", false, "Use TLS encryption for communication")
+
 	flag.Usage = func() { fmt.Print(usage) }
 	flag.Parse()
 	log.SetFlags(log.Lshortfile)
 
 	port = ":" + port
-	if serve {
 
-		ln, err := net.Listen("tcp", port)
+	var ln net.Listener
+	var err error
+
+	if encrypted {
+		home := os.Getenv("HOME")
+		cer, err := tls.LoadX509KeyPair(home+"/.httpecho/server.crt", home+"/.httpecho/server.key")
 		if err != nil {
 			log.Println(err)
 			return
 		}
-		defer ln.Close()
+		config := &tls.Config{Certificates: []tls.Certificate{cer}}
+		ln, err = tls.Listen("tcp", port, config)
+	} else {
+		ln, err = net.Listen("tcp", port)
+	}
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer ln.Close()
+	if serve {
+
 		for {
 			conn, err := ln.Accept()
 			conn.SetDeadline(time.Now().Add(time.Duration(timeout) * time.Second)) //close http request
@@ -64,12 +86,6 @@ func main() {
 		}
 
 	} else { //only 1 time
-		ln, err := net.Listen("tcp", ":8888")
-		if err != nil {
-			log.Println(err)
-			return
-		}
-		defer ln.Close()
 
 		conn, err := ln.Accept()
 		//conn.SetDeadline(time.Now().Add(time.Duration(timeout) * time.Millisecond)) //close http request
@@ -122,7 +138,7 @@ func handleConnection(conn net.Conn, dump string, timeout int) {
 			request += msg
 		}
 		//handle read error
-		if err != nil {
+		if err != nil && err != io.EOF {
 			if !strings.Contains(err.Error(), "timeout") && !strings.Contains(err.Error(), "closed network connection") { //avoid timeout error
 				log.Println(err)
 			}
